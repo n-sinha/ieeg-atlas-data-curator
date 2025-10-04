c9813a5b2497569a47b4274a3cab58c7c90baf72
Nishant Sinha <nishant.sinha89@gmail.com>
2025-10-03 20:19:44 -0400

    get minimum time before stimulation onset

--- scipts/ram/sync_ram.py
+++ scipts/ram/sync_ram.py
@@ -15,17 +15,32 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
 
+def is_not_empty(value):
+    """Check if a value is not empty/missing"""
+    if pd.isna(value):  # Handles NaN, None
+        return False
+    if isinstance(value, str):
+        return value.strip().lower() not in ['n/a', 'na', '', 'nan', 'null']
+    return True
+
 #%%
 
 def main():
     # get all events.tsv files in the data/output/ram directory
     events_tsv_files = list(Path("data/output/ram").rglob("**/*ieeg*/*_events.tsv"))
     logging.info(f"Found {len(events_tsv_files)} events.tsv files")
+    start_task_onsets = []
     for events_tsv_file in events_tsv_files:
         # read the events.tsv file
         events = pd.read_csv(events_tsv_file, sep="\t")
-        stim = events['stim_file'].unique()
-        logging.info(f"Stim: {stim} for {events_tsv_file.name}")
+        # find the row where the value of stim_file is not empty
+        non_empty_stim = events[events['stim_file'].apply(is_not_empty)]
+        # find the location of the first non-empty stim_file and use that to get the onset
+        start_task_onset = non_empty_stim['onset'].iloc[0]
+        start_task_onsets.append(start_task_onset)
+        logging.info(f"Onset: {start_task_onset} for {events_tsv_file.name}")
+
+    logging.info(f"Minimum start task onset: {min(start_task_onsets)}")
 
 #%%
 
